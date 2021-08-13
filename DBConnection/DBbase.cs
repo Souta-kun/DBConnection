94c51c46872474c53260c01dbc82b3d082b5a1b5
DESKTOP-LHKUCJ0\CARLOS BARRIOS <cbarrios@asesoftware.com>
2021-08-13 17:14:59 -0500

    no message

---
+++ DBConnection/DBbase.cs
@@ -0,0 +1,33 @@
+﻿using Insight.Database;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DBConnection
+{
+    public class DBbase
+    {
+        static DBbase()
+        {
+            SqlInsightDbProvider.RegisterProvider();
+        }
+
+        public static SqlConnection Connection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Connection.Database"].ConnectionString;
+
+            SqlConnection conn;
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return conn;
+        }
+    }
+}
